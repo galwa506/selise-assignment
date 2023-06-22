0af3c495f74cc7d21e5c2a0a4cfb6eeb417bac82
Galeyee <galwang173@gmail.com>
2023-06-23 01:53:42 +0600

    lesson-4-created-an-interface

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
-
+import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -22,6 +22,16 @@ import { CommonModule } from '@angular/common';
   `,
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: 'assets/example-house.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
